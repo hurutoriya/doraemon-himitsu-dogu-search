024b116c40be6e36adf96e338488ce4202bbc259
Shunya Ueta <hurutoriya@users.noreply.github.com>
2022-10-29 22:02:45 +0900

    add(hybrid saerch mapping)

--- doraemon_himitsu_dogu_search/data.py
+++ doraemon_himitsu_dogu_search/data.py
@@ -6,7 +6,7 @@ ELASTIC_PASSWORD = "elastic"
 CERT_PATH = "http_ca.crt"
 ENDPOINT = "https://localhost:9200"
 INDEX_NAME = "himitsu_dogu"
-MAPPING_PATH = "data/input/ann_mapping.json"
+MAPPING_PATH = "data/input/hybridsearch_mapping.json"
 # NOTE: Data path
 SENTENTS_VECTOR_DATA_PATH: str = "data/output/himitsu_dogu_sentens_vector.npy"
 HIMITSU_DOGU_RAW_DATA_PATH: str = "./data/raw/himitsu-dogu.txt"
